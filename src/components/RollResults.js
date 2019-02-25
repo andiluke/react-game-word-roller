06c2d9e446ca0293909515773645a1f64c0b15fd
Andi Luke <andiluke@gmail.com>
2019-02-24 17:13:23 -0800

    a bit more style

--- src/components/RollResults.js
+++ src/components/RollResults.js
@@ -4,11 +4,15 @@ class ShowResults extends Component {
     render() {
         return (
             <div className="roll_results">
-                <div className="roll_results_header">Is it a word?</div>
                 <div className="result_maybe_word_images">
                 {this.props.results.map((letter, index) => (
                     <img src={'images/letters/' + letter + '.png'} alt={letter} key={index} />
                 ))}
+                </div>
+                <div className="roll_results_header">Is it a word?</div>
+                <div className="roll_results_buttons_wrap">
+                    <button className="roll_result_button">Yes</button>
+                    <button className="roll_result_button">No</button>
                 </div>
             </div>
         );

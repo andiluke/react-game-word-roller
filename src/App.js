d37efd0494a894dfb2c7655a8e055b4a6e80ca02
Andi Luke <andiluke@gmail.com>
2019-02-27 07:19:37 -0800

    dictionary list, good enough

--- src/App.js
+++ src/App.js
@@ -5,7 +5,47 @@ import LetterDice from './components/LetterDice.js';
 import RollResults from './components/RollResults.js';
 import Header from './components/Header.js';
 import Welcome from './components/Welcome.js';
+/*
+sat, sad, saw, sap,
+set, sew, 
+sit, sip, sin,
+sob, sod, sow, son
+sud, sum, sun, sup
+tab, tad, tan, tap
+ten,
+tin, tip,
+tot, tow, ton, top
+tub
+bat, bad, ban,
+bet, bed,
+bit, bid, bin,
+bot, bob, bow, bop,
+but, bud, bum, bun,
+dab, dad, dam, 
+den,
+did, dim, din, dip,
+dot, don,
+dub, dud,
+cat, cab, cad, can, cap,
+cot, cob, cow, cop,
+cut, cub, cud, cup,
+rat, rad, raw, ram, ran, rap,
+red,
+rib, rid, rim, rip,
+rot, rob, rod, row, 
+rut, rub, run,
+mat, mad, maw, man, map,
+met, men,
+mit, 
+mob, mow, mop,
+mud, 
+nab, nap,
+net, new, 
+nit, nib, nip,
+not, nod, now, 
+nut
 
+*/
 class App extends Component {
   
   constructor(props) {

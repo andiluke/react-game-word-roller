5d47b2b5b01c34e9694323f3af505222cb878b3b
Andi Luke <andiluke@gmail.com>
2019-02-26 19:33:17 -0800

    collecting user name

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,14 +3,26 @@ import React from 'react';
   const Welcome = (props) => {
     const handleBtnClick = () => {
         let username = document.getElementById('welcome_username').value;
-        console.log('handleBtnClick,', username);
         props.setUserName(username);
     }
-    return (
+
+    const EnterName = (props) => (
         <div className="welcome">
             <h2>Hello! What is your name?</h2>
-            <input id="welcome_username" /> <button onClick={handleBtnClick}>Go</button>
+            <input id="welcome_username" data-lpignore="true" /> <button onClick={handleBtnClick}>Go</button>
         </div>
     );
+
+    const DisplayInfo = (props) => (
+        <div className="display_info_wrap">
+            Hi {props.username}! Your score is: {props.score}
+        </div>
+    );
+
+    if (props.username) {
+        return <DisplayInfo username={props.username} score={props.score}/>;
+    } else {
+        return <EnterName />;
+    }
   }
   export default Welcome;
